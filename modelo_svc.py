0c9ce9e2189be69e90847194cfb2bc80b400843f
renatoreggiani <renatoreggiani@gmail.com>
2021-09-17 23:10:02 -0300

    Update modelo_svc.py

--- modelo_svc.py
+++ modelo_svc.py
@@ -22,7 +22,7 @@ X, y = get_datas(scaler=scaler)
 
 #%%
 
-model = SVC()
+model = SVC(C=1)
 
 SEED = 51
 
@@ -43,13 +43,15 @@ print('Media: ', scores.mean(),
 #%%
 
 params = {
-    'C': range(1, 151, 10),
+    'C': range(1, 201, 3),
     'kernel':['linear', 'poly', 'rbf', 'sigmoid'],
-    'gamma':['scale', 'auto'],
+    'gamma':['scale', 'auto', 0.5, 1.5],
     # 'degree': range(1,10, 2),
+    'shrinking': [True, False]
     }
 
-clf = RandomizedSearchCV(model, params, random_state=SEED, n_iter=50, cv=ss, n_jobs=-1)
+clf = RandomizedSearchCV(model, params, random_state=SEED, n_iter=10, cv=ss,
+                         scoring=['accuracy', 'f1'], n_jobs=-1)
 
 rsearch = clf.fit(X, y)
 rsearch.best_params_
@@ -65,7 +67,7 @@ model = SVC(**rsearch.best_params_)
 
 SEED = 666
 
-ss = ShuffleSplit(n_splits=100, test_size=0.25, random_state=SEED)
+ss = ShuffleSplit(n_splits=300, test_size=0.25, random_state=SEED)
 
 cv = ss
 
